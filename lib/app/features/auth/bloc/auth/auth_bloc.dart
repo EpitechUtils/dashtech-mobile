5c877a7d641a4bc6845e5f58dff651439054bb82
Cyril Colinet <cyrilcolinet.pro@gmail.com>
2020-10-16 10:40:28 +0200

    feat(security): Refacto security in backend

--- lib/app/features/auth/bloc/auth/auth_bloc.dart
+++ lib/app/features/auth/bloc/auth/auth_bloc.dart
@@ -1,8 +1,6 @@
-import 'dart:convert';
-
 import 'package:basic_utils/basic_utils.dart';
-import 'package:crypto/crypto.dart';
 import 'package:epitech_intranet_mobile/app/core/firebase/firebase_notifications.dart';
+import 'package:epitech_intranet_mobile/app/core/utils/device_utils.dart';
 import 'package:epitech_intranet_mobile/app/features/auth/bloc/auth/auth_event.dart';
 import 'package:epitech_intranet_mobile/app/features/auth/bloc/auth/auth_state.dart';
 import 'package:epitech_intranet_mobile/app/features/auth/business/use_cases/logout_usecase.dart';
@@ -12,7 +10,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:injectable/injectable.dart';
-import 'package:uuid/uuid.dart';
 
 @injectable
 @lazySingleton
@@ -40,22 +37,11 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
   }
 
   Stream<AuthState> _mapAppStartedToState() async* {
-    final String uuidVal = await secureStorage.read(key: 'uuid');
-    final String secureHash = await secureStorage.read(key: 'secureHash');
-    if (StringUtils.isNullOrEmpty(uuidVal)) {
-      secureStorage.write(key: 'uuid', value: Uuid().v4());
-    }
-
-    // Create secure hash
-    if (StringUtils.isNullOrEmpty(secureHash)) {
-      final bytes = utf8.encode(Uuid().v4());
-      final digest = sha512.convert(bytes);
-      secureStorage.write(key: 'secureHash', value: digest.toString());
-    }
-
-    FirebaseNotifications().listen(uuidVal);
+    final String uuidVal = (await DeviceUtils.getDeviceDetails())['identifier'];
     final String profileId = await secureStorage.read(key: 'profileId');
+    FirebaseNotifications().listen(uuidVal);
     await Future.delayed(const Duration(seconds: 5), () {});
+
     if (StringUtils.isNotNullOrEmpty(profileId)) {
       //profileBloc.add(ProfileEvent.forceLoadProfile());
       yield AuthState.authenticated();
